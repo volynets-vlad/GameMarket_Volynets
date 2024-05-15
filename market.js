7f3131d6c3187640ba0aa0c48a921a1c9be6b5d8
volynets-vlad <158173204+volynets-vlad@users.noreply.github.com>
2024-05-15 12:16:41 +0300

    123 но чуть краще

--- market.js
+++ market.js
@@ -15,11 +15,36 @@ if (itemsDiv) {
     console.log('не знайдено')
 }
 
-//viznachennya masivu
-let itemsArray = ['', '', '', '']
-//vivedennya masivu v consol
+
 console.log(itemsArray)
-//vivedennya elementiv masivu v consol
-for (let i = 0; i < itemsArray.length; i++) {
-    console.log(itemsArray[i])
+
+let itemsArray = [
+    'Газонокосарка 43',
+    'Електричний тример 110',
+    'Електрична газонокосарка 32',
+    'Акумуляторний оприскувач 12 ',
+    'Газонокосарка 43',
+    'Електричний тример 110',
+    'Електрична газонокосарка 30',
+    'Акумуляторний оприскувач 10 ',
+    'Газонокосарка 44',
+    'Електричний тример 111',
+    'Електрична газонокосарка 31',
+    'Акумуляторний оприскувач 13'
+];
+
+
+if (itemsDiv) {
+    itemsArray.forEach((item, index) => {
+        let itemNumber = (index + 1).toString().padStart(2, ''); 
+        itemsDiv.innerHTML +=
+        `
+        <div class="item">
+            <h2>товар №${index+1} з ${itemsArray.length}</h2>
+            <p>${item}</p>
+        </div>
+        `;
+    });
+} else {
+    console.log('Блок товарів не знайдено');
 }
