bc89c52f36501526ac2098fb6600e69d002cb826
volynets-vlad <158173204+volynets-vlad@users.noreply.github.com>
2024-05-15 12:27:51 +0300

    123 ще раз

--- market.js
+++ market.js
@@ -16,7 +16,7 @@ if (itemsDiv) {
 }
 
 
-console.log(itemsArray)
+
 
 let itemsArray = [
     'Газонокосарка 43',
@@ -32,19 +32,19 @@ let itemsArray = [
     'Електрична газонокосарка 31',
     'Акумуляторний оприскувач 13'
 ];
+console.log(itemsArray)
 
+/*.forEach((item, index) =>{
+    //Виводимо на веб сторінку елемент масиву в блок з класом іtеm
+    itemsDiv.innerHTML +=
 
-if (itemsDiv) {
-    itemsArray.forEach((item, index) => {
-        let itemNumber = (index + 1).toString().padStart(2, ''); 
-        itemsDiv.innerHTML +=
-        `
-        <div class="item">
-            <h2>товар №${index+1} з ${itemsArray.length}</h2>
-            <p>${item}</p>
-        </div>
-        `;
-    });
-} else {
-    console.log('Блок товарів не знайдено');
-}
+    <div class= "item">
+<h2>Вчитель№ ${index + 1} з ${itemsArray.length}</h2>
+<p>${item.firstName} ${item.lastName}</p>
+<p>Предмет: ${item.subject}</p>
+<p><img src= "${item.photo}" class="item-image"></p>
+<p><a href = "${item.url}" target="_blank" class=
+"bonus price ">Профіль</a></p>
+
+</div>
+})*/
