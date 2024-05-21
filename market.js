307725044f3d0df4fa7fa6a89c4b8c59613fe88d
volynets-vlad <158173204+volynets-vlad@users.noreply.github.com>
2024-05-21 15:36:32 +0300

    шось трошки не то, але не знаю шо, перерив і перепробував все

--- market.js
+++ market.js
@@ -9,7 +9,7 @@ let itemsDiv = document.getElementById("items");
 
 
 
-let itemsArray = [
+/* let itemsArray = [
     {
         firsName:"Віталій",
         lastName:"Шатківський",
@@ -146,7 +146,45 @@ let itemsArray = [
         photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/hecha-olena-anatoliyivna-683x1024.jpg",
         url:"https://lyceum.ztu.edu.ua/team/hecha-o-a/",
     },
-];
+];*/
+let itemsArray = [
+    {
+        title: "Мотокоса 43",
+        image: 'img-market/1.png',
+        mono: 6,
+        pb: 8,
+        prise_standart: 5499,
+        prise_discount: 4497,
+        prise_coupon: 4395,
+    },
+    {
+        title: "Електричний тример 110",
+        image: 'https://static.dnipro-m.ua/origin/description/6566/design/m-110.jpg',
+        mono: 6,
+        pb: 8,
+        prise_standart: 4497,
+        prise_discount: 3498,
+        prise_coupon: 3396,
+    },
+    {
+        title: "Електрична газонокосарка 32",
+        image: 'https://images.prom.ua/3772468812_w640_h640_gazonokosilka-elektricheskaya-dnipro-m.jpg',
+        mono: 6,
+        pb: 8,
+        prise_standart: 4497,
+        prise_discount: 3297,
+        prise_coupon: 3185,
+    },
+    {
+        title: "Акумуляторний обприскувач 12 N",
+        image: 'https://hotline.ua/img/tx/449/4492289865.jpg',
+        mono: 6,
+        pb: 8,
+        prise_standart: 2190,
+        prise_discount: 1698,
+        prise_coupon: 1596,
+    },
+]
 console.log(itemsArray)
 
 //Перевірка існування знайденого блоку
@@ -165,7 +203,7 @@ console.log(itemsArray)
     console.log('Блок товарів не знайдено');
 }*/
 
-if (itemsDiv) {
+/*if (itemsDiv) {
     itemsArray
     .forEach((item, index) => {
         itemsDiv.innerHTML +=
@@ -179,4 +217,42 @@ if (itemsDiv) {
             <p><a href="${item.url}" target="_blank" class="bonus price">Профіль</a></p>
         </div>
         `;
-    });}
+    });}*/
+if (itemsDiv) {
+    itemsArray
+        .forEach((item, index) => {
+            itemsDiv.innerHTML +=
+            `
+            <!==Блок окремого товару-->
+            <div class="item">
+
+                <div class="item-title">${item.title}</div>
+
+                <!--Блок зображення товару-->
+                <div class="item-image">
+                    <img src="${item.image}" class="item-image"
+                </div>
+    
+                <!--Блок з оплатою частинами-->
+                <div class="parts-pay">
+                    <div><img scr="img-market/mono-lapka.png" alt="">${item.mono}</div>
+                    <div><img src="img-market/pb.jpg" alt="">${item.pd}</div>
+                </div>
+
+                <!--Блок ціни-->
+                <div class="prise">
+                    <div><span>${item.prise_standart}</span><sup>грн</sup><div>
+                    <div><span>${item.prise_discount}</span><sup>грн</sup><div>
+                </div>
+    
+                <!--Блок бонусної ціни -->
+                <div class="prise bonus">
+                    <div>ціна за купоном</div>
+                    <div><span>${item.prise_coupon}</span><sup>грн</sup></div>
+                </div>
+            </div>
+            `
+        })
+} else {
+    console.log('Блок товарів не знайдено')
+}
